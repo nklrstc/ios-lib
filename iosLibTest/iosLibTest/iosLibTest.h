6b288bdcafb5956e0e546de94413f3b90ba0d41a
Nikola Ristic <nristic@equinoxpayments.com>
2023-06-14 14:40:15 +0200

    ios-lib added

---
+++ iosLibTest/iosLibTest/iosLibTest.h
@@ -0,0 +1,16 @@
+//
+//  iosLibTest.h
+//  iosLibTest
+//
+//  Created by Pavle Milojevic on 13.6.23..
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iosLibTest : NSObject
+
+
+- (void) test1;
+- (NSString*) test2: (NSString*) param;
+
+@end
---
+++ iosLibTest/iosLibTest/iosLibTest.m
@@ -0,0 +1,27 @@
+//
+//  iosLibTest.m
+//  iosLibTest
+//
+//  Created by Pavle Milojevic on 13.6.23..
+//
+
+#import "iosLibTest.h"
+
+@implementation iosLibTest
+
+- (void) test1 {
+    NSLog(@"test1 called");
+};
+
+
+- (NSString*) test2: (NSString*) param {
+    NSLog(@"test2 called");
+    return param;
+};
+
+
+- (void) test3 {
+    NSLog(@"test3 called");
+};
+
+@end
